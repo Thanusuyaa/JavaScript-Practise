9fba24a0e93a6218ad6134d83b95d3b63433c89d
Thanusuyaa <tanuselvakumar197@gmail.com>
2024-02-29 12:45:49 +0530

    calculator

--- index.js
+++ index.js
@@ -71,33 +71,83 @@
 
 
 // <!-- 6) Chat app -->
- function sendsms()
- { 
-    value1=document.getElementById("i1").value;
-    value2=document.getElementById("i2").value;
-    element=document.createElement("li");
+//  function sendsms()
+//  { 
+//     value1=document.getElementById("i1").value;
+//     value2=document.getElementById("i2").value;
+//     element=document.createElement("li");
 
-    if(value1 !="")
-    {
-        text1=document.createTextNode(value1);
-        element.appendChild(text1);
-        mydiv=document.getElementById("mydiv");
-        mydiv.appendChild(element);
-        element.style.color="#FDB7B9";
-        document.getElementById("i1").value="";
+//     if(value1 !="")
+//     {
+//         text1=document.createTextNode(value1);
+//         element.appendChild(text1);
+//         mydiv=document.getElementById("mydiv");
+//         mydiv.appendChild(element);
+//         element.style.color="blue";
+//         document.getElementById("i1").value="";
+//     }
+//     else if(value2 !="")
+//     {
+//         text2=document.createTextNode(value2);
+//         element.appendChild(text2);
+//         mydiv=document.getElementById("mydiv");
+//         mydiv.appendChild(element);
+//         element.style.color="red";
+//         document.getElementById("i2").value="";
+//     }
+ 
+//  }
+
+// 7) Calculator
+function send(i){
+    x=document.getElementById("input1");
+    switch(i){
+        case 0:x.value+=0;
+        break;
+        case 1:x.value+=1;
+        break;
+        case 2:x.value+=2;
+        break;
+        case 3:x.value+=3;
+        break;
+        case 4:x.value+=4;
+        break;
+        case 5:x.value+=5;
+        break;
+        case 6:x.value+=6;
+        break;
+        case 7:x.value+=7;
+        break;
+        case 8:x.value+=8;
+        break;
+        case 9:x.value+=9;
+        break;
+ 
+        case '+':x.value+='+'; break;
+        case '-':x.value+='-'; break;
+        case '/': x.value+='/'; break;
+        case '*':x.value+='*'; break;
+        case '.':x.value+='.'; break;
+        case 'AC':x.value="";break;
+
+        case '=': result=eval(x.value);
+        x.value=result; 
+        break;
+         
+        case 'log': result=eval(Math.log10(x.value));
+        x.value=result;
+        break;
+
+        case '': result=eval(Math.sqrt(x.value));
+        x.value=result;
+        break;
+
+        case '1/x': result=eval(1/x.value);
+        x.value=result;
+        break;
+
     }
-    else if(value2 !="")
-    {
-        text2=document.createTextNode(value2);
-        element.appendChild(text2);
-        mydiv=document.getElementById("mydiv");
-        mydiv.appendChild(element);
-        element.style.color="#d8f3e9";
-        document.getElementById("i2").value="";
-    }
-
- }
-
+}
 
 
 
